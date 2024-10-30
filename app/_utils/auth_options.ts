10f430664466401c7053b3ba2c6fe7f8cf8a7736
unknown <htikewaikyawmmocc@gmail.com>
2024-10-30 15:41:32 +0630

    fix auth-options

--- app/_utils/auth_options.ts
+++ app/_utils/auth_options.ts
@@ -1,17 +1,29 @@
-import NextAuth, { Session, NextAuthOptions } from 'next-auth';
+import { Session } from 'next-auth';
+import type { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+
 import { JWT } from 'next-auth/jwt';
+const maxAge = 60 * 60; //1hour
 
 export const authOptions: NextAuthOptions = {
+  // CredentialsProviderの場合 adapter は使用できない模様。
+  //adapter: PrismaAdapter(prisma),
+  theme: {
+    colorScheme: 'light',
+  },
   providers: [
     CredentialsProvider({
-      // Name the provider
-      name: 'Credentials',
+      id: 'credentials',
+      name: 'credentials',
       credentials: {
-        email: { label: 'Username', type: 'text' },
-        password: { label: 'Password', type: 'password' },
+        email: {
+          label: 'Email',
+          type: 'text',
+          placeholder: 'Email',
+        },
+        password: { label: 'Password', type: 'password', placeholder: 'Password' },
       },
-      async authorize(credentials) {
+      authorize: async (credentials, req) => {
         const url = process.env.LOGIN_API_LINK as string;
         const response = await fetch(url, {
           method: 'POST',
@@ -22,6 +34,7 @@ export const authOptions: NextAuthOptions = {
           cache: 'no-store',
         });
         const result = await response.json();
+
         if (result.message === 'success') {
           return result.data;
         }
@@ -29,29 +42,43 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-
   session: {
-    strategy: 'jwt',
+    maxAge: maxAge,
   },
   callbacks: {
     async jwt({ token, user }: { token: JWT; user: any }) {
+      // 最初のサインイン
       if (user) {
-        token.id = user.id;
+        return {
+          ...token,
+          name: user.username,
+          email: user.email,
+          // employeeId: user.employeeId,
+          // masterUserGroup: user.masterUserGroup,
+        };
       }
       return token;
     },
-    // async session({ session, token }) {
-    //   session.user.id = token.id;
-    //   return session;
-    // },
-    async session({ session, token }) {
-      if (token) {
-        session.user = {
-          id: token.id as string,
-          name: token.name as string,
-        };
-      }
+    async session({ session, token }: { session: Session; token: JWT }) {
+      // session.user.id = token.sub;
+      session.user.name = token.name;
+      session.user.email = token.email;
       return session;
     },
+    // redirect url for multi domain usage
+    async redirect({ url, baseUrl }) {
+      // console.log("url",new Date().toISOString(),url),
+      // console.log("baseUrl",new Date().toISOString(),baseUrl)
+      if (url.startsWith('/')) return url;
+      return url;
+    },
   },
+
+  secret: process.env.NEXTAUTH_SECRET,
+  // サインイン・サインアウトで飛ぶカスタムログインページを指定
+  // pages: {
+  //   signIn: '/auth/signin',
+  // },
+  // Enable debug messages in the console if you are having problems
+  debug: process.env.NODE_ENV === 'development',
 };
