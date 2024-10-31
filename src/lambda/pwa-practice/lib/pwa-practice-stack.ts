5f7e0365585860c44cf12716b6671904c0601d7f
unknown <htikewaikyawmmocc@gmail.com>
2024-10-31 10:10:37 +0630

    add cdk stacks

--- src/lambda/pwa-practice/lib/pwa-practice-stack.ts
+++ src/lambda/pwa-practice/lib/pwa-practice-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PwaPracticeStack extends cdk.Stack {
@@ -12,5 +14,20 @@ export class PwaPracticeStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'PwaPracticeQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const UserApiFunction = new lambda.Function(this, 'UserApi', {
+      functionName:'UserApiFunction',
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('src/pwa-practice/user/lambda'),
+    });
+    
+    const OperationsApiFunction = new lambda.Function(this, 'OperationsApi', {
+      functionName:'OperationsApiFunction',
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('src/pwa-practice/operations/lambda'),
+    });
+    
   }
 }
---
+++ src/lambda/pwa-practice/src/pwa-practice/operations/lambda/index.ts
@@ -0,0 +1,17 @@
+import { Handler, APIGatewayEvent } from 'aws-lambda';
+
+export const handler: Handler = async (event: APIGatewayEvent) => {
+    console.log('operations.....',event);
+    return buildResponse(200, { message: 'success operations.....' });
+}
+
+// build response
+const buildResponse = (statusCode: Number, body: Object): Object => {
+    return {
+        statusCode,
+        headers: {
+            "ContentType": "Application/Json"
+        },
+        body: JSON.stringify(body)
+    }
+};
